862ceb61a8efdcdc617192fbb84f004343c91709
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-31 13:16:02 -0500

    Add colors and font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,32 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   app: {
     head: {
       htmlAttrs: {
         lang: "en",
       },
-      title: "Room Homepage",
+      title: "Frontend Mentor | Room Homepage",
     },
+  },
+
+  modules: ["@nuxt/fonts"],
+  fonts: {
+    families: [
+      {
+        name: "League Spartan",
+        provider: "google",
+        weights: [500, 600, 700],
+        styles: ["normal"],
+        subsets: ["latin"],
+        preload: true,
+        display: "swap",
+      },
+    ],
   },
 });

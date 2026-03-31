db2a684dd64727e86b22519b827a7c13ce2f6802
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-31 13:19:38 -0500

    Add favicon and metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,20 +5,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   app: {
     head: {
       htmlAttrs: {
         lang: "en",
       },
       title: "Frontend Mentor | Room Homepage",
+      meta: [{ name: "description", content: "A Frontend Mentor challenge." }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon-32x32.png" }],
     },
   },
-
   modules: ["@nuxt/fonts"],
   fonts: {
     families: [

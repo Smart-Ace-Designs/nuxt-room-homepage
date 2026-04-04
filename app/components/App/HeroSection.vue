36a56b37744ccd3663933dcfd8c76d043edb6a0b
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-03 21:01:23 -0500

    Fix padding

--- app/components/App/HeroSection.vue
+++ app/components/App/HeroSection.vue
@@ -52,7 +52,7 @@ const currentImageIndex = ref(0);
         </button>
       </div>
     </div>
-    <div class="min-w-0 bg-theme-white px-8 py-14 2xl:px-24 2xl:pt-30">
+    <div class="min-w-0 bg-theme-white px-8 py-16 2xl:px-24 2xl:pt-30">
       <h1
         class="mb-4 text-[2.45rem] leading-9 font-semibold tracking-tighter xl:text-5xl xl:leading-11"
       >
